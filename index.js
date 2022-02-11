1c7cfefca8e5119b74d4b23af8e617e40820339a
Supaasara Bowman <supaasarabowman@Supaasaras-MacBook-Pro.local>
2022-02-11 13:10:15 -0700

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,47 @@
 // Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendCat(name) {
+    return cats.unshift(name);
+}
+
+function destructivelyPrependCat(name) {
+    return cats.unshift(name);
+}
+
+function destructivelyAppendCat(name) {
+    cats.pop();
+}
+
+function destructivelyRemoveFirstCat() {
+    cats.shift();
+}
+
+function appendCat(name) {
+    return [...cats,name];
+}
+
+function destructivelyAppendCat(name) {
+    cats.push("Ralph")   
+}
+
+function destructivelyRemoveLastCat() {
+    cats.pop()
+}
+
+function prependCat(name) {
+    return cats.slice()
+}
+
+function prependCat(name) {
+    return [name, ...cats] 
+}
+
+function removeFirstCat() {
+    return cats.slice(1);
+}
+
+function removeLastCat() {
+    return cats.slice(0.1,2)
+}
+
